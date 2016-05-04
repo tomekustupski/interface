c0ed20296b1a388df45b7c79dceb11bfa8e91308
EC2 Default User <ec2-user@ip-172-31-23-157.eu-central-1.compute.internal>
2016-05-04 15:57:40 +0000

    update

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+import os
+from flask import Flask, render_template, jsonify
 app = Flask(__name__)
 
 @app.route("/hello")
@@ -8,6 +9,15 @@ def hello():
 @app.route("/")
 def index():
   return render_template('upload_form.html', uploadButtonName="send")
+
+@app.route("/upload", methods=['POST'])
+def upload():
+  files = request.files
+  for f in files.getlist('file'):
+    filename = f.filename
+    updir = '/home/ec2-user/s3-2/upload'
+    f.save(os.path.join(updir, filename))
+  return jsonify()
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
